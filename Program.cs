222b8b1a0484e46cc912c7bb46d2c8884cc12dbf
Thomas Eichinger <ThomasEichy@live.com>
2021-02-04 23:39:17 -0600

    Setup Alice Method

--- Program.cs
+++ Program.cs
@@ -91,12 +91,29 @@ namespace CS_Console_App
 
         public void Alice()
         {
-            Console.WriteLine("Alice");
+            string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the" +
+            " book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures " +
+            "or conversation?'";
+
+            Console.WriteLine("\nPlease enter a term from the first sentence of Alice in Wonderland and I'll see " +
+            "if it is truely there!");
+            string term = Console.ReadLine().ToLower();
+
+            if (alice.ToLower().Contains(term))
+            {
+                Console.WriteLine("\nYes this term exists in the first sentence.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nIm sorry, this term does not exist in the first sentence.\n");
+            }
+
+            Intro();
         }
         
         public void Quit()
         {
-            Console.WriteLine("Thank you for using our app. Goodbye!");
+            Console.WriteLine("\nThank you for using our app. Goodbye!");
             return;
         }
 
