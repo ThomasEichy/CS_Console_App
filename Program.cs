cc0a6fc08b42dd180c430c965242736f3ee9dc7e
Thomas Eichinger <ThomasEichy@live.com>
2021-02-04 22:45:31 -0600

    Setup main menu.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,82 @@ namespace CS_Console_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu menuInst = new Menu();
+
+            menuInst.Intro();
+            
         }
+    }
+
+    class Menu
+    {
+
+        public void Intro()
+        {
+            Console.WriteLine("Hello! Please enter the application name you would" +
+            " like to use. \nYour choices are below. \n\nName Greeting | Rectagle Area" +
+             " Calculator | Gas Mileage Calculator | Alice Search");
+
+            string menuSelect = Console.ReadLine().ToLower();
+            
+            if (menuSelect == "name greeting")
+            {
+                Name();
+            }
+            else if (menuSelect == "rectagle area calculator")
+            {
+                RectArea();
+            }
+            else if (menuSelect == "gas mileage calculator")
+            {
+                GasMiles();
+            }
+            else if (menuSelect == "alice search")
+            {
+                Alice();
+            }
+            else if (menuSelect == "quit")
+            {
+                Quit();
+            }
+            else
+            {
+                Err();
+            }
+        }
+
+        public void Name()
+        {
+            Console.WriteLine("Name");
+        }
+
+        public void RectArea()
+        {
+            Console.WriteLine("Area");
+        }
+
+        public void GasMiles()
+        {
+            Console.WriteLine("Miles");
+        }
+
+        public void Alice()
+        {
+            Console.WriteLine("Alice");
+        }
+        
+        public void Quit()
+        {
+            Console.WriteLine("Thank you for using our app. Goodbye!");
+            return;
+        }
+
+        public void Err()
+        {
+            Console.WriteLine("\nI'm sorry, this option is not valid. \nPlease try typing again.\n");
+            Intro();
+
+        }
+    
     }
 }
